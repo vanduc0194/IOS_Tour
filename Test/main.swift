1484b9d7b75e6072efbba3826bf15d9728be48b5
Nguyễn Văn Đức <vanduc0194@gmail.com>
2016-11-24 09:13:47 +0700

    Initial Commit

---
+++ Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Test
+//
+//  Created by NguyenVanDuc on 11/24/16.
+//  Copyright © 2016 NguyenVanDuc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
