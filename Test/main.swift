1d6fd7830f34e64a4e62988597de41e08a9eea0d
Nguyen Van Duc <vanduc0194@gmail.com>
2016-12-14 09:14:42 +0700

    Test

--- Test/main.swift
+++ Test/main.swift
@@ -10,3 +10,18 @@ import Foundation
 
 print("Hello, World!")
 
+class StudentMarks {
+    // Khai báo thuộc tính
+    var diemToan = 8
+    var diemLy = 9
+    var diemHoa = 10
+}
+
+
+// Tạo thể hiện (object)
+let sm = StudentMarks()
+
+// Truy cập các thuộc tính của class
+print("Điểm toán là \(sm.diemToan)")
+print("Điểm lý là \(sm.diemLy)")
+print("Điểm hoá là \(sm.diemHoa)")
